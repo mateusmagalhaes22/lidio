1427122e7521134e523e142eafce6d70da64bcac
mateusmagalhaes22 <magalhaes.mateus22@gmail.com>
2025-07-27 15:00:34 -0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Lidio</h1>
+      <p>Your platform for selling grease trap covers and more.</p>
+    </div>
+  );
+}
+
+export default Home;
